9bd4daf563527bb5a274a5a781072847842fc6c8
Victor0491 <victorlsepulveda@hotmail.com>
2024-04-26 19:56:49 -0400

    Prueba creación de componente card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
