473805139845fac5e2b5a8df7cdfb54ef3982f23
Victor0491 <victorlsepulveda@hotmail.com>
2024-04-26 20:39:53 -0400

    Component card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+  @Input() imageUrl!: string;
+  @Input() title!: string;
+  @Input() references!: string;
+  @Input() date!: string;
+  @Input() price!: string;
 }
