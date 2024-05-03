86ea49ff2208b0fb4b5fd9113c47d7a1070493db
Victor0491 <victorlsepulveda@hotmail.com>
2024-05-03 03:54:05 -0400

    Se agrega carrusel a la pantalla principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CardComponent } from './shared/card/card.component';
+import { CarruselComponent } from './shared/carrusel/carrusel.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent,CardComponent,CommonModule],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,CardComponent,CommonModule,CarruselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -10,14 +10,64 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  @Input() imageUrl!: string;
-  @Input() title!: string;
-  @Input() references!: string;
-  @Input() date!: string;
-  @Input() price!: string;
-
-  alojamientos= [ 
+  @Input() alojamientos: any[] = [
     {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    {
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'Valparaiso',
+      imagenes: ['https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWxvamFtaWVudG9zJTIwcGxheWF8ZW58MHx8MHx8fDA%3D', 'https://plus.unsplash.com/premium_photo-1680287296835-0424869199ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFsb2phbWllbnRvcyUyMHBsYXlhfGVufDB8fDB8fHww', 'https://images.unsplash.com/photo-1495822892661-2ead864e1c7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D','https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjQ1fHxhbG9qYW1pZW50b3MlMjBwbGF5YXxlbnwwfHwwfHx8MA%3D%3D']
+    },
+    /*{
       imagen: "https://images.unsplash.com/photo-1609319172668-8b4f021f3b7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGFsb2phbWllbnRvc3xlbnwwfHwwfHx8MA%3D%3D",
       nombre: 'Casa en el bosque',
       descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
@@ -44,6 +94,6 @@ export class CardComponent {
       descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
       precio: 200000,
       region: 'La Araucanía',
-    },
+    },*/
   ]
 }
