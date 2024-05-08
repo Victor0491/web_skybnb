7c2f196307ae4b4f058fac9098c0524c885643d0
JorgeFuentes13 <jo.fuentess@duocuc.cl>
2024-05-07 21:08:26 -0400

    navbar login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarAuthComponent } from '../../shared/components/nav-bar-auth/nav-bar-auth.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NavBarAuthComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavBarAuthComponent } from '../../shared/components/nav-bar-auth/nav-bar-auth.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NavBarAuthComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
