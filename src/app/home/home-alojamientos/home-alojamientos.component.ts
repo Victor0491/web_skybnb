a69b657e34d75c162c7685b696ee07abcae0b5a9
JorgeFuentes13 <jo.fuentess@duocuc.cl>
2024-05-07 22:35:25 -0400

    Fixes merge

--- src/app/home/home-alojamientos/home-alojamientos.component.ts
+++ src/app/home/home-alojamientos/home-alojamientos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CarruselComponent } from '../../shared/components/carrusel/carrusel.component';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-home-alojamientos',
   standalone: true,
-  imports: [],
+  imports: [CardComponent,CarruselComponent],
   templateUrl: './home-alojamientos.component.html',
   styleUrl: './home-alojamientos.component.css'
 })
