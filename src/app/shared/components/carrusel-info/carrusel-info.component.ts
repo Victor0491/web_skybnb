5c8585c26d68207cf5c49926faddb9176eb3c86f
Jenni <je.oyarce@duocuc.cl>
2024-05-10 20:49:02 -0400

    Pagina detalle alojamientos

---
+++ src/app/shared/components/carrusel-info/carrusel-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrusel-info.component.html',
+  styleUrl: './carrusel-info.component.css'
+})
+export class CarruselInfoComponent {
+
+}
