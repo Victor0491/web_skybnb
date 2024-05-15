66b37936f84e2359177f65e3c3d6d16126eff1bd
JorgeFuentes13 <jo.fuentess@duocuc.cl>
2024-05-15 00:42:27 -0400

    logica login , validator

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarAuthComponent } from '../../shared/components/nav-bar-auth/nav-bar-auth.component';
+import { RouterModule } from '@angular/router';
+import { User } from '../../core/models/User';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,NavBarAuthComponent],
+  imports: [CommonModule,NavBarAuthComponent,RouterModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  user: User = {
+    correo : '',
+    password :'',
+  }
+
+  show_error = false;
+  mensaje_error : string = '';
+
+
+  validarForm(){
+    if (this.user.correo && this.user.password){
+      
+    } else {
+      this.show_error = true;
+      this.mensaje_error = 'Por favor complete los campos'
+    }
+  }
+
+  login(){
+    this.validarForm();
+  }
+
 }
+
