24a8cc51eb2daee26915563e9609f63f66927878
Jenni <je.oyarce@duocuc.cl>
2024-05-19 18:33:18 -0400

    Pagina detalle alojamientos, mapa

--- src/app/alojamientos/alojamiento-detail/alojamiento-detail.component.ts
+++ src/app/alojamientos/alojamiento-detail/alojamiento-detail.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ModalInformationComponent } from '../../shared/components/modal-information/modal-information.component';
 import { CarruselInfoComponent } from '../../shared/components/carrusel-info/carrusel-info.component';
+import { MapaComponent } from '../../shared/components/mapa/mapa.component';
 
 
 @Component({
   selector: 'app-alojamiento-detail',
   standalone: true,
-  imports: [ModalInformationComponent, CarruselInfoComponent],
+  imports: [ModalInformationComponent, CarruselInfoComponent, MapaComponent],
   templateUrl: './alojamiento-detail.component.html',
   styleUrl: './alojamiento-detail.component.css'
 })
