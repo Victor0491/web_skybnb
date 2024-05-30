645f3d0f342e37fc704e12f562c7e39dfa09394c
Victor0491 <victorlsepulveda@hotmail.com>
2024-05-29 20:57:03 -0400

    Actualización vista Perfil de usuario

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,46 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [RouterLink, CommonModule,MatExpansionModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
 
   user: any = {
+    username: 'Mihawk',
     name: 'John Doe',
     lastName: 'Smith',
-    email: 'john.doe@email.com'
+    email: 'john.doe@email.com',
+    phone: ''
   };
   accommodations: any[] = [
     {
-      title: 'Cozy Apartment in Downtown',
-      description: 'A charming one-bedroom apartment in the heart of the city.',
-      location: 'New York, NY',
-      pricePerNight: 100
+      nombre: 'Casa en la Playa',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 60000,
+      region: 'Valparaiso',
     },
     {
-      title: 'Beachfront Villa with Pool',
-      description: 'Relax and enjoy the ocean views from this luxurious villa.',
-      location: 'Miami, FL',
-      pricePerNight: 250
+      nombre: 'Cabaña en el bosque Huilo Huilo',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      precio: 100000,
+      region: 'De los Rios',
     }
   ];
   reservations: any[] = [
     {
-      title: 'Cozy Apartment in Downtown',
+      nombre: 'Cabaña en el bosque Huilo Huilo',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      region: 'De los Rios',
       startDate: '2024-06-01',
       endDate: '2024-06-05'
     },
     {
-      title: 'Beachfront Villa with Pool',
-      startDate: '2024-07-10',
-      endDate: '2024-07-15'
+      nombre: 'Cabaña en el bosque Huilo Huilo',
+      descripcion: 'Casa cercana a tus lugares favoritos con hermosa vista al paisaje natural de tu preferencia',
+      region: 'De los Rios',
+      startDate: '2023-07-20',
+      endDate: '2023-07-27'
     }
   ];
 
