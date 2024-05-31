6ca5a90a5ed7fe3714fb2dca966c6c0cc0b094fe
Jenni <je.oyarce@duocuc.cl>
2024-05-30 22:54:32 -0400

    Arreglos

--- src/app/alojamientos/alojamiento-detail/alojamiento-detail.component.ts
+++ src/app/alojamientos/alojamiento-detail/alojamiento-detail.component.ts
@@ -3,14 +3,22 @@ import { ModalInformationComponent } from '../../shared/components/modal-informa
 import { CarruselInfoComponent } from '../../shared/components/carrusel-info/carrusel-info.component';
 import { MapaComponent } from '../../shared/components/mapa/mapa.component';
 
-
 @Component({
   selector: 'app-alojamiento-detail',
   standalone: true,
   imports: [ModalInformationComponent, CarruselInfoComponent, MapaComponent],
   templateUrl: './alojamiento-detail.component.html',
-  styleUrl: './alojamiento-detail.component.css'
+  styleUrls: ['./alojamiento-detail.component.css']
 })
 export class AlojamientoDetailComponent {
 
+  compartir() {
+    // Lógica para compartir
+    console.log('Compartir clicked');
+  }
+
+  guardar() {
+    // Lógica para guardar
+    console.log('Guardar clicked');
+  }
 }
