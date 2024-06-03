87f68ebe3b55ddc7f7a1e6e0c39eee7c2224fdf8
MatiasCalisto <matias.calisto2@gmail.com>
2024-06-03 15:10:24 -0400

    modificacion para el ingreso de alojamientos nuevo css

--- src/app/anfitrion/paso1/actividad/actividad.component.ts
+++ src/app/anfitrion/paso1/actividad/actividad.component.ts
@@ -1,43 +1,63 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-actividad',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './actividad.component.html',
   styleUrls: ['./actividad.component.css']
 })
 export class ActividadComponent {
-  nuevoAlojamiento: any = {}; // Objeto para almacenar los datos del alojamiento
-  opcionesSeleccionadas: string[] = [];
-
-  constructor(private router: Router) {}
-
-  seleccionarOpcion(opcion: string): void {
-    if (this.opcionesSeleccionadas.includes(opcion)) {
-      this.opcionesSeleccionadas = this.opcionesSeleccionadas.filter(item => item !== opcion);
-    } else {
-      const maxOpciones = 3; // Número máximo de opciones permitidas
-      if (this.opcionesSeleccionadas.length < maxOpciones) {
-        this.opcionesSeleccionadas.push(opcion);
+  preferenciasForm: FormGroup;
+  actividades: { nombre: string, imagen: string }[] = [
+    { nombre: 'Surf', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQt0BnL44w1stL-X2MFG0SjvWJJIUIaSJo0Q&s' },
+    { nombre: 'Buceo', imagen: 'https://s3-us-west-2.amazonaws.com/wp-divingyucatan/wp-content/uploads/2020/04/15110254/tipos-trajes-buceo.jpg' },
+    { nombre: 'Escalada en roca', imagen: 'https://www.culturarecreacionydeporte.gov.co/sites/default/files/styles/870_x_540/public/2023-06/climbing-g42cb58814_640.jpg?itok=19O-jOqD' },
+    { nombre: 'Senderismo', imagen: 'https://www.webconsultas.com/sites/default/files/styles/wc_adaptive_image__medium/public/media/2020/07/15/senderismo_p.jpg' },
+    { nombre: 'Caminata al aire libre', imagen: 'https://www.bupasalud.com/sites/default/files/styles/640_x_400/public/articulos/2020-04/fotos/caminata-salud.jpg?itok=ny1gDa_0' },
+    { nombre: 'Prefiero Omitir', imagen: 'https://media.istockphoto.com/id/540861476/es/foto/relajaci%C3%B3n-total.jpg?s=612x612&w=0&k=20&c=GEoRJR5eCnHUoV62GH52mNnSO_LnzDzG_AMpPLjpNbE=' }
+  ];
+  seccionActual = 'actividad';
+  
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.preferenciasForm = this.fb.group({
+      actividad: [[], [Validators.required, Validators.minLength(1), Validators.maxLength(3)]]
+    });
+  }
+  
+  navigateToUbicacion(actividad: string): void {
+    const actividadesControl = this.preferenciasForm.get('actividad');
+    if (actividadesControl && actividadesControl.value) {
+      const actividades = actividadesControl.value;
+      if (actividades.includes(actividad)) {
+        actividadesControl.setValue(actividades.filter((a: string) => a !== actividad));
+      } else if (actividades.length < 3) {
+        actividadesControl.setValue([...actividades, actividad]);
       } else {
-        alert("Solo puedes seleccionar hasta tres opciones.");
+        // Manejar el caso donde ya se han seleccionado 3 actividades
+        alert('Solo puedes seleccionar hasta 3 actividades.');
       }
     }
   }
 
-  navigateToUbicacion(): void {
-    if (this.opcionesSeleccionadas.length === 0) {
-      alert('Debe seleccionar por lo menos una opción');
-      return;
+  navigateToPaso2(): void {
+    if (this.preferenciasForm.get('actividad')?.value.length <= 3) {
+      this.router.navigate(['anfitrion/paso2']);
+    } else {
+      alert('Solo puedes seleccionar hasta 3 actividades.');
     }
-    // Redirige a la página de ubicación y pasa el objeto nuevoAlojamiento
-    this.router.navigate(['anfitrion/paso2'], { state: { alojamiento: this.nuevoAlojamiento } });
   }
 
   navigateToEntorno(): void {
-    // Redirige a la página de ubicación y pasa el objeto nuevoAlojamiento
-    this.router.navigate(['anfitrion/entorno'], { state: { alojamiento: this.nuevoAlojamiento } });
+    this.router.navigate(['anfitrion/entorno']);
   }
 }
--- src/app/anfitrion/paso1/descripcion/descripcion.component.ts
+++ src/app/anfitrion/paso1/descripcion/descripcion.component.ts
@@ -1,28 +1,45 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-descripcion',
   templateUrl: './descripcion.component.html',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,
+            CommonModule,
+            ReactiveFormsModule,],
   styleUrls: ['./descripcion.component.css']
 })
 export class DescripcionComponent {
-  nuevoAlojamiento: any = {}; // Objeto para almacenar los datos del alojamiento
+  preferenciasForm: FormGroup;
+  tiposAlojamiento: { nombre: string, imagen: string }[] = [
+    { nombre: 'Cabaña', imagen: 'https://www.shutterstock.com/image-photo/wooden-cottage-forest-near-biogradsko-600nw-1963746835.jpg' },
+    { nombre: 'Casa', imagen: 'https://st2.depositphotos.com/1041088/11595/i/450/depositphotos_115954550-stock-photo-home-exterior-with-garage-and.jpg' },
+    { nombre: 'Departamento', imagen: 'https://http2.mlstatic.com/D_NQ_NP_766438-MLC75223256781_032024-O.webp' }
+  ];
 
-  constructor(private router: Router) {}
+  seccionActual = 'tipoAlojamiento';
+  historialSecciones: string[] = [];
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.preferenciasForm = this.fb.group({
+      tipoAlojamiento: ['', Validators.required],
+    });
+  }
 
-  seleccionarOpcion(opcion: string) {
-    this.nuevoAlojamiento.tipo_alojamiento = opcion; // Guarda el tipo de alojamiento seleccionado
+  seleccionarTipo(tipo: string): void {
+    this.preferenciasForm.get('tipoAlojamiento')?.setValue(tipo);
   }
 
   navigateToUbicacion() {
     // Redirige a la página de ubicación y pasa el objeto nuevoAlojamiento
-    this.router.navigate(['anfitrion/entorno'], { state: { alojamiento: this.nuevoAlojamiento } });
+    this.router.navigate(['anfitrion/entorno']);
   }
   navigateToPaso1() {
     // Redirige a la página de ubicación y pasa el objeto nuevoAlojamiento
-    this.router.navigate(['anfitrion/paso1'], { state: { alojamiento: this.nuevoAlojamiento } });
+    this.router.navigate(['anfitrion/paso1']);
   }
 }
