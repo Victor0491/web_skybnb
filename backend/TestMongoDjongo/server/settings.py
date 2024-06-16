ab90720f1cf3d320c8088b91bd280c2613358f7f
corpsebride19 <je.oyarce@duococ.clm>
2024-06-16 00:28:14 -0400

    cambios alojamientos

--- backend/TestMongoDjongo/server/settings.py
+++ backend/TestMongoDjongo/server/settings.py
@@ -83,14 +83,11 @@ DATABASES = {
             'NAME': 'Skybnb',
             'ENFORCE_SCHEMA': False,
             'CLIENT': {
-<<<<<<< HEAD
 
                 'host': 'mongodb+srv://matiascalisto:Sca2021-@skybnb.g3rbwz5.mongodb.net/?retryWrites=true&w=majority&appName=Skybnb'
 
-=======
-                'host': 'mongodb+srv://matiascalisto:Sca2021-@skybnb.g3rbwz5.mongodb.net/?retryWrites=true&w=majority&appName=Skybnb'
->>>>>>> victor
-            }  
+        
+                  }  
         }
 }
 
