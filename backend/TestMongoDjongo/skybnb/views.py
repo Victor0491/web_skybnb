99cb89b075a6b599536042aa2a5b9323a14a924d
JorgeFuentes13 <jo.fuentess@duocuc.cl>
2024-06-22 22:25:32 -0400

    crear alojamiento

--- backend/TestMongoDjongo/skybnb/views.py
+++ backend/TestMongoDjongo/skybnb/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth import authenticate, login
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,6 +18,7 @@ from .serializers import GetPerfilUsuarioSerializers, GetAlojamientoSerializers,
 
 #------------------------Register--Login-------------------------------------------
 class RegisterAPIView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = UsuarioSerializers(data=request.data)
         if serializer.is_valid():
@@ -26,6 +29,7 @@ class RegisterAPIView(APIView):
 
 
 class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
@@ -47,19 +51,23 @@ class LoginAPIView(APIView):
 class ServiciosList(generics.ListAPIView):
     queryset = Servicios.objects.all()
     serializer_class = ServiciosSerializers
+    permission_classes = [AllowAny]
 
 class ServiciosCreateView(generics.CreateAPIView):
     queryset = Servicios.objects.all()
     serializer_class = ServiciosSerializers
+    permission_classes = [AllowAny]
 
 #---------------------ACTIVIDAD----------------------------
 class ActividadCreateView(generics.CreateAPIView):
     queryset = Actividades.objects.all()
     serializer_class = ActividadSerializers
+    permission_classes = [AllowAny]
 
 class ActividadList(generics.ListAPIView):
     queryset = Actividades.objects.all()
     serializer_class = ActividadSerializers
+    permission_classes = [AllowAny]
 
 
 
@@ -68,10 +76,12 @@ class ActividadList(generics.ListAPIView):
 class UbicacionCreateView(generics.CreateAPIView):
     queryset = Ubicacion.objects.all()
     serializer_class = UbicacionSerializers
+    permission_classes = [AllowAny]
 
 class UbicacionList(generics.ListAPIView):
     queryset = Ubicacion.objects.all()
     serializer_class = UbicacionSerializers
+    permission_classes = [AllowAny]
 
 
 #------------------------TIPO-ALOJAMIENTO--------------------------------
@@ -79,10 +89,12 @@ class UbicacionList(generics.ListAPIView):
 class TipoAlojamientoCreateView(generics.CreateAPIView):
     queryset = TipoAlojamiento.objects.all()
     serializer_class = TipoAlojamientoSerializers
+    permission_classes = [AllowAny]
 
 class TipoAlojamientoList(generics.ListAPIView):
     queryset = TipoAlojamiento.objects.all()
     serializer_class = TipoAlojamientoSerializers
+    permission_classes = [AllowAny]
 
 
 #-----------------------ROL CREATE-------------------------------
@@ -90,6 +102,7 @@ class TipoAlojamientoList(generics.ListAPIView):
 class RolCreateView(generics.CreateAPIView):
     queryset = Rol.objects.all()
     serializer_class = RolesSerializers
+    permission_classes = [AllowAny]
 
 
 #-----------------------USUARIO----------------------
@@ -98,46 +111,63 @@ class RolCreateView(generics.CreateAPIView):
 class UsuarioCreateView(generics.CreateAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializers
+    permission_classes = [AllowAny]
 
 #------------------------PERFIL USUARIO--------------------------------
 
 class PerfilUsuarioCreateView(generics.CreateAPIView):
     queryset = PerfilUsuario.objects.all()
     serializer_class = PerfilUsuarioSerializers
+    permission_classes = [AllowAny]
 
 #GET ID
 class PerfilUsuarioDetail(generics.RetrieveAPIView):
     queryset = PerfilUsuario.objects.all()
     serializer_class = GetPerfilUsuarioSerializers
     lookup_field = 'usuario_id'
+    permission_classes = [IsAuthenticated]
 
+class PerfilUsuarioUpdateAPIView(generics.UpdateAPIView):
+    queryset = PerfilUsuario.objects.all()
+    serializer_class = PerfilUsuarioSerializers
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        # Asume que el perfil pertenece al usuario autenticado
+        return self.request.user.perfil
 #----------------------Alojamiento----------------------------------------
 
 class AlojamientoCreateView(generics.CreateAPIView):
     queryset = Alojamiento.objects.all()
     serializer_class = AlojamientoSerializers
+    permission_classes = [AllowAny]
 
 class GetAlojamiento(generics.ListAPIView):
     queryset = Alojamiento.objects.all()
     serializer_class = GetAlojamientoSerializers
+    permission_classes = [AllowAny]
 
 class GetDetailsAlojamiento(generics.RetrieveAPIView):
     queryset = Alojamiento.objects.all()
     serializer_class = GetDetailsAlojamientoSerializers
+    permission_classes = [AllowAny]
 
 
 class AlojamientoViewSet(generics.CreateAPIView):
     queryset = Alojamiento.objects.all()
     serializer_class = ImagenAlojamientoSerializer
+    permission_classes = [AllowAny]
 #----------------------------------------------------------------------------
 
 class ReservaCreateView(generics.CreateAPIView):
     queryset = Reserva.objects.all()
     serializer_class = ReservaSerializers
+    permission_classes = [AllowAny]
 
 class ReservaListUser(generics.ListAPIView):
     queryset = Reserva
     serializer_class = ReservaListSerializers
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         usuario_id = self.kwargs['usuario_id']
