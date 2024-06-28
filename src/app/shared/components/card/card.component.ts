5b37cb102d9c089bb85c21e40ee41705a7d48990
MatiasCalisto <matias.calisto2@gmail.com>
2024-06-28 17:55:05 -0400

    Modificacion preferencias

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ListAlojamiento } from '../../../core/models/Alojamiento';
@@ -6,7 +6,7 @@ import { AlojamientoService } from '../../../core/service/alojamiento/alojamient
 import { ObjectToArrayPipe } from '../../../core/pipe/object-to-array.pipe';
 import { TruncatePipe } from '../../../core/pipe/truncate.pipe';
 import { SeparadorMilesPipe } from '../../../core/pipe/separador-miles.pipe';
-import { SkeletonLoaderComponent } from '../skeleton-loader/skeleton-loader.component'; // Asegúrate de que la ruta es correcta
+import { SkeletonLoaderComponent } from '../skeleton-loader/skeleton-loader.component';
 
 @Component({
   selector: 'app-card',
@@ -16,10 +16,10 @@ import { SkeletonLoaderComponent } from '../skeleton-loader/skeleton-loader.comp
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
-  isLoading = true; // Agregado para el estado de carga
-  isLoaded = false; // Agregado para el control de la animación
+  @Input() recomendados: any[] = [];
   alojamientos: any[] = [];
+  isLoading = true;
+  isLoaded = false;
 
   constructor(private alojamientoService: AlojamientoService, private cd: ChangeDetectorRef) { }
 
@@ -32,10 +32,9 @@ export class CardComponent implements OnInit {
       this.alojamientos = data;
       this.isLoading = false;
       setTimeout(() => {
-        this.isLoaded = true; // Cambia el estado de animación una vez que los datos se han cargado
-        this.cd.detectChanges(); // Detectar cambios manualmente
+        this.isLoaded = true;
+        this.cd.detectChanges();
       }, 0);
-      console.log(this.alojamientos);
     });
   }
 }
